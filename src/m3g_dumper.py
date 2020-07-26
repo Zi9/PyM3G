8d4e02d707f4361485fbbd8dd0f8bca9b459f259
Zi9 <kevin.lumik@gmail.com>
2020-07-26 14:48:35 +0300

    Format with black

--- src/m3g_dumper.py
+++ src/m3g_dumper.py
@@ -6,7 +6,7 @@ from typing import List, Dict
 import timeit
 
 ObjTypeNames = {
-    0: 'Header',
+    0: "Header",
     1: "AnimationController",
     2: "AnimationTrack",
     3: "Appearance",
@@ -29,7 +29,7 @@ ObjTypeNames = {
     20: "VertexArray",
     21: "VertexBuffer",
     22: "World",
-    255: "External Reference"
+    255: "External Reference",
 }
 
 
@@ -37,6 +37,7 @@ ObjTypeNames = {
 class Object3D:
     """An abstract base class for all objects that can be part of a 3D
     world. """
+
     user_id: int
     animation_tracks: List[int] = []
     user_parameters: Dict = {}
@@ -46,6 +47,7 @@ class Object3D:
 class Transformable(Object3D):
     """An abstract base class for Node and Texture2D, defining common methods
     for manipulating node and texture transformations."""
+
     has_component_transform: bool
     translation: tuple = None
     scale: tuple = None
@@ -58,6 +60,7 @@ class Transformable(Object3D):
 @dataclass
 class Node(Transformable):
     """An abstract base class for all scene graph nodes."""
+
     enable_rendering: bool
     enable_picking: bool
     alpha_factor: int
@@ -72,6 +75,7 @@ class Node(Transformable):
 @dataclass
 class Header:
     """Header object"""  # TODO: Proper docstring
+
     version_number: tuple = None
     has_external_references: bool = None
     total_file_size: int = None
@@ -82,6 +86,7 @@ class Header:
 @dataclass
 class AnimationController(Object3D):
     """Controls the position, speed and weight of an animation sequence."""
+
     speed: float
     weight: float
     active_interval_start: int
@@ -94,6 +99,7 @@ class AnimationController(Object3D):
 class AnimationTrack(Object3D):
     """Associates a KeyframeSequence with an AnimationController and an
     animatable property."""
+
     keyframe_sequence: int
     animation_controller: int
     property_id: int
@@ -103,6 +109,7 @@ class AnimationTrack(Object3D):
 class Appearance(Object3D):
     """A set of component objects that define the rendering attributes of a
     Mesh or Sprite3D."""
+
     layer: int
     compositing_mode: int
     fog: int
@@ -114,6 +121,7 @@ class Appearance(Object3D):
 @dataclass
 class Background(Object3D):
     """Defines whether and how to clear the viewport."""
+
     background_color: tuple
     background_image: int
     background_image_mode_x: int
@@ -128,14 +136,15 @@ class Background(Object3D):
 
 class Loader:
     """Base reader class for m3g files"""
-    M3G_Signature = b'\xAB\x4A\x53\x52\x31\x38\x34\xBB\x0D\x0A\x1A\x0A'
+
+    M3G_Signature = b"\xAB\x4A\x53\x52\x31\x38\x34\xBB\x0D\x0A\x1A\x0A"
 
     objects = []
 
     def __init__(self, path):
-        self.file = open(path, 'rb')
+        self.file = open(path, "rb")
         if self.VerifySignature():
-            print('Got m3g file')
+            print("Got m3g file")
             self.ReadSections()
         self.file.close()
 
@@ -146,17 +155,17 @@ class Loader:
         return False
 
     def ReadObject3D(self, rdr):
-        uid, aCount = unpack('<II', rdr.read(8))
+        uid, aCount = unpack("<II", rdr.read(8))
         if aCount > 0:
-            animTracks = unpack('<' + str(aCount) + 'I', rdr.read(aCount*4))
+            animTracks = unpack("<" + str(aCount) + "I", rdr.read(aCount * 4))
         else:
             animTracks = None
-        uparamCount = unpack('<I', rdr.read(4))[0]
+        uparamCount = unpack("<I", rdr.read(4))[0]
         if uparamCount > 0:
             params = []
             for i in range(uparamCount):
-                paramID, sz = unpack('<II', rdr.read(8))
-                val = unpack('<' + str(sz) + 'c')
+                paramID, sz = unpack("<II", rdr.read(8))
+                val = unpack("<" + str(sz) + "c")
                 params.append((paramID, val))
         else:
             params = None
@@ -164,24 +173,32 @@ class Loader:
 
     def ReadTransformable(self, rdr):
         sup = self.ReadObject3D(rdr)
-        compTransf = unpack('<?', rdr.read(1))
+        compTransf = unpack("<?", rdr.read(1))
         if compTransf:
-            translation = unpack('<fff', rdr.read(12))
-            scale = unpack('<fff', rdr.read(12))
-            orientAngle = unpack('<f', rdr.read(4))[0]
-            orientAxis = unpack('<fff', rdr.read(12))
-        genTransf = unpack('<?', rdr.read(1))[0]
+            translation = unpack("<fff", rdr.read(12))
+            scale = unpack("<fff", rdr.read(12))
+            orientAngle = unpack("<f", rdr.read(4))[0]
+            orientAxis = unpack("<fff", rdr.read(12))
+        genTransf = unpack("<?", rdr.read(1))[0]
         if genTransf:
-            mtrx = unpack('<16f', rdr.read(64))
-        tr = Transformable(sup, compTransf, translation, scale, orientAngle,
-                           orientAxis, genTransf, mtrx)
+            mtrx = unpack("<16f", rdr.read(64))
+        tr = Transformable(
+            sup,
+            compTransf,
+            translation,
+            scale,
+            orientAngle,
+            orientAxis,
+            genTransf,
+            mtrx,
+        )
         return tr
 
     def ReadNode(self, rdr):
         sup = self.ReadTransformable(rdr)
-        render, pick, alpha, scope, align = unpack('<??BI?', rdr.read(8))
+        render, pick, alpha, scope, align = unpack("<??BI?", rdr.read(8))
         if align:
-            zt, yt, zr, yr = unpack('<BBII', rdr.read(10))
+            zt, yt, zr, yr = unpack("<BBII", rdr.read(10))
         else:
             zt = None
             yt = None
@@ -193,27 +210,27 @@ class Loader:
     def ParseObject(self, objtype, data):
         rdr = BytesIO(data)
         if objtype == 0:
-            verMa, verMi, refs, size = unpack('<BB?I4x', rdr.read(11))
-            authoring = rdr.read().rstrip(b'\x00').decode('utf-8')
-            hdr = Header(f'{verMa}.{verMi}', size, refs, authoring)
+            verMa, verMi, refs, size = unpack("<BB?I4x", rdr.read(11))
+            authoring = rdr.read().rstrip(b"\x00").decode("utf-8")
+            hdr = Header(f"{verMa}.{verMi}", size, refs, authoring)
             rdr.close()
             return hdr
         elif objtype == 8:
             s_class = self.ReadObject3D(rdr)
-            d = unpack('<BBB???', rdr.read(6))
+            d = unpack("<BBB???", rdr.read(6))
             pmode = PolygonMode(s_class, d[0], d[1], d[2], d[3], d[4], d[5])
             rdr.close()
             return pmode
         elif objtype == 17:
             s_class = self.ReadTransformable(rdr)
-            img = unpack('<I', rdr.read(4))[0]
-            bcol = unpack('<BBB', rdr.read(3))
-            blend, wrpS, wrpT, lFil, iFil = unpack('<5B', rdr.read(5))
+            img = unpack("<I", rdr.read(4))[0]
+            bcol = unpack("<BBB", rdr.read(3))
+            blend, wrpS, wrpT, lFil, iFil = unpack("<5B", rdr.read(5))
             t2d = Texture2D(s_class, img, bcol, blend, wrpS, wrpT, lFil, iFil)
             rdr.close()
             return t2d
         elif objtype == 255:
-            uri = rdr.read().rstrip(b'\x00').decode('utf-8')
+            uri = rdr.read().rstrip(b"\x00").decode("utf-8")
             rdr.close()
             return ExternalReference(uri)
 
@@ -224,20 +241,20 @@ class Loader:
         rdr = BytesIO(data)
         while True:
             objectHeader = rdr.read(5)
-            if objectHeader == b'':
+            if objectHeader == b"":
                 break
-            objectType, sz = unpack('<BI', objectHeader)
-            print(f'Got object of type {ObjTypeNames[objectType]}')
+            objectType, sz = unpack("<BI", objectHeader)
+            print(f"Got object of type {ObjTypeNames[objectType]}")
             self.objects.append(self.ParseObject(objectType, rdr.read(sz)))
         rdr.close()
 
     def ReadSections(self):
         while True:
             sectionHeader = self.file.read(9)
-            if sectionHeader == b'':
+            if sectionHeader == b"":
                 break
-            print(f'Section data starting at {self.file.tell()}')
-            compression, totalLen, uncomp = unpack('<BII', sectionHeader)
+            print(f"Section data starting at {self.file.tell()}")
+            compression, totalLen, uncomp = unpack("<BII", sectionHeader)
             sectionLength = totalLen - 13
             self.ReadObjects(self.file.read(sectionLength))
             self.file.read(4)
@@ -245,11 +262,11 @@ class Loader:
 
 start = timeit.default_timer()
 
-a = Loader('../testfiles/vrally/car_subaru.m3g')
+a = Loader("../testfiles/vrally/car_subaru.m3g")
 # a = Loader('/home/zi/tmp/ug/car_exotic.m3g')
 
-print(f'Loaded file in {timeit.default_timer()-start} seconds')
+print(f"Loaded file in {timeit.default_timer()-start} seconds")
 
-print('Found the following objects in file:')
+print("Found the following objects in file:")
 for o in a.objects:
-    print(f'  {o}')
+    print(f"  {o}")
