7dc88e16de1d35baeaafa6a7ffff840ed4037e29
Zi9 <kevin.lumik@gmail.com>
2020-09-05 21:44:04 +0300

    Clean, implemented 2 new readers

--- dumpall.py
+++ dumpall.py
@@ -1,8 +1,6 @@
 """M3G Dumper utility - dumps contents of JSR 184 3d files"""
 from sys import argv
 import timeit
-import sys
-sys.path.append(".")
 import m3g
 
 start = timeit.default_timer()
@@ -13,19 +11,8 @@ index = 1
 for o in a.objects:
     if not isinstance(o, str):
         print(f"{index} - {o}")
-        index += 1
-for o in a.objects:
-    if isinstance(o, str):
+    else:
         print(f"Missing reader for {o}")
+    index += 1
 
-a = m3g.Loader(argv[2])
-stop = timeit.default_timer()
-index = 1
-for o in a.objects:
-    if not isinstance(o, str):
-        print(f"{index} - {o}")
-        index += 1
-for o in a.objects:
-    if isinstance(o, str):
-        print(f"Missing reader for {o}")
 print(f"Loaded file in {round(stop-start, 5)} seconds")
--- dumpmesh.py
+++ dumpmesh.py
@@ -1,20 +1,24 @@
 """M3G dumpmesh utility - dumps all meshes from a  JSR 184 3d file"""
 from sys import argv
-import m3g
+import m3g_resolve
 
-# a = m3g.Loader(argv[1])
-a = m3g.Loader("../testfiles/vrally80/car_peugeot.m3g")
-meshes = []
-outf = open("out.obj", "w")
+a = m3g_resolve.Loader(argv[1])
+# a = m3g_resolve.Loader("testfiles/vrally80/car_peugeot.m3g")
+outname = argv[1].split("/")[-1].replace(".m3g", ".obj")
+if len(argv) == 3:
+    outfolder = argv[2] + "/"
+else:
+    outfolder = ""
+outf = open(outfolder + outname, "w")
 for ob in a.objects:
-    if isinstance(ob, m3g.Mesh):
+    if isinstance(ob, m3g_resolve.Mesh):
         outf.write("o test\n")
         for i in ob.vertex_buffer.positions.vertices:
             outf.write(f"v {i[0]/100} {i[1]/100} {i[2]/100}\n")
         for i in ob.vertex_buffer.tex_coords[0].vertices:
             outf.write("vt {0} {1}\n".format(
-                round(i[0]/255, 5),
-                round(i[1]/255, 5)
+                round(i[0]/128, 5) + 0.01,
+                round(i[1]/128, 5) * -1 + 1
             ))
         for i in range(0, len(ob.index_buffer[0].indices), 3):
             outf.write("f {0}/{0} {1}/{1} {2}/{2}\n".format(
@@ -22,8 +26,4 @@ for ob in a.objects:
                 ob.index_buffer[0].indices[i+1]+1,
                 ob.index_buffer[0].indices[i+2]+1
             ))
-        # meshes.append(ob)
-
-
-# print(meshes[0])
-# print(meshes[0].vertex_buffer.positions)
+outf.close()
