74e68ee000adbff85d46f2afc99a71c9a6864d62
Zi9 <17174057+Zi9@users.noreply.github.com>
2020-11-16 16:07:59 +0200

    Write function for Object3D

--- PyM3G/objects/base.py
+++ PyM3G/objects/base.py
@@ -1,7 +1,7 @@
 """
 Contains base classes for objects
 """
-from struct import unpack
+from struct import unpack, pack
 
 
 class Object3D:
@@ -16,13 +16,28 @@ class Object3D:
         self.user_parameters = {}
 
     def read(self, reader):
+        """Read object data from an input stream"""
         self.user_id, at_count = unpack("<II", reader.read(8))
-        for _ in range(at_count):
-            self.animation_tracks.append(unpack("<I", reader.read(4))[0])
+        if at_count > 0:
+            for _ in range(at_count):
+                self.animation_tracks.append(unpack("<I", reader.read(4))[0])
         up_count = unpack("<I", reader.read(4))[0]
-        for _ in range(up_count):
-            pid, psz = unpack("<II", reader.read(8))
-            self.user_parameters[pid] = reader.read(psz)
+        if up_count > 0:
+            for _ in range(up_count):
+                pid, psz = unpack("<II", reader.read(8))
+                self.user_parameters[pid] = reader.read(psz)
+
+    def write(self, writer):
+        """Write object data to an output stream"""
+        writer.write(pack("<II", self.user_id, len(self.animation_tracks)))
+        if len(self.animation_tracks) > 0:
+            for track in self.animation_tracks:
+                writer.write(pack("<I", track))
+        writer.write(pack("<I", len(self.user_parameters)))
+        if len(self.user_parameters) > 0:
+            for pid, pval in self.user_parameters.items():
+                writer.write(pack("<II", pid, len(pval)))
+                writer.write(pval)
 
 
 class Transformable(Object3D):
